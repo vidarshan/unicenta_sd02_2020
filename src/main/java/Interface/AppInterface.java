fc8971f5e08964b089b45cef127a239e984718b3
Savindu Bashitha <savibashnon11@gmail.com>
2020-04-30 11:24:38 +0530

    navbar update

--- src/main/java/Interface/AppInterface.java
+++ src/main/java/Interface/AppInterface.java
@@ -95,8 +95,8 @@ public class AppInterface extends javax.swing.JFrame {
                      else
                          d_n = "AM";
                      
-                     jLabel3.setText("" + hour+" : " + min + " : " + second + " "+d_n);
-                     jLabel2.setText("" + day + " - " + month + " - " + year + "");
+                     jLabel3.setText(" " + hour+" : " + min + " : " + second + " "+d_n);
+                     jLabel2.setText("" + day + " - " + month + " - " + year + " ");
                      sleep(1000);
                  }
              }catch(Exception e){
@@ -153,8 +153,8 @@ public class AppInterface extends javax.swing.JFrame {
         jSeparator6 = new javax.swing.JSeparator();
         jSeparator7 = new javax.swing.JSeparator();
         jPanel2 = new javax.swing.JPanel();
-        navbar = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
+        navbar = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         contentLayout = new javax.swing.JLayeredPane();
 
@@ -331,42 +331,26 @@ public class AppInterface extends javax.swing.JFrame {
 
         jPanel2.setBackground(new java.awt.Color(55, 71, 79));
         jPanel2.setForeground(new java.awt.Color(255, 255, 255));
+        jPanel2.setLayout(new java.awt.GridLayout());
+
+        jLabel3.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel3.setFont(new java.awt.Font("Century", 1, 20)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("jLabel3");
+        jPanel2.add(jLabel3);
 
         navbar.setBackground(new java.awt.Color(0, 51, 51));
         navbar.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         navbar.setForeground(new java.awt.Color(255, 255, 255));
         navbar.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         navbar.setText("SALES");
+        jPanel2.add(navbar);
 
-        jLabel3.setBackground(new java.awt.Color(255, 255, 255));
-        jLabel3.setFont(new java.awt.Font("Century", 0, 24)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setText("jLabel3");
-
-        jLabel2.setFont(new java.awt.Font("Century", 0, 24)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Century", 1, 20)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.TRAILING);
         jLabel2.setText("jLabel2");
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 223, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(navbar, javax.swing.GroupLayout.DEFAULT_SIZE, 1121, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 223, javax.swing.GroupLayout.PREFERRED_SIZE))
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                .addComponent(navbar, javax.swing.GroupLayout.DEFAULT_SIZE, 46, Short.MAX_VALUE)
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE))
-        );
+        jPanel2.add(jLabel2);
 
         contentLayout.setBackground(new java.awt.Color(28, 35, 51));
         contentLayout.setLayout(new java.awt.CardLayout());
